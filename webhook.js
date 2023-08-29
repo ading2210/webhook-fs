e92f3edfa4ba8c47aad52612d5e5d4afe36ca8de
ading2210 <ading2019@gmail.com>
2023-08-29 04:26:21 -0400

    allow attachment uploading

--- webhook.js
+++ webhook.js
@@ -1,16 +1,21 @@
-async function execute_webhook(webhook_url, content, attachments=[]) {
+async function execute_webhook(webhook_url, content, attachments={}) {
   webhook_url += "?wait=true";
   let payload = {
     "content": content
   }
-  
+
   //use multipart form data for attachments
   let form_data = new FormData();
-  //todo: attach files
+  let file_num = 1;
+  for (let [filename, attachment] of Object.entries(attachments)) {
+    form_data.append(`file${file_num}`, attachment, filename);
+    file_num ++;
+  }
   form_data.append("payload_json", JSON.stringify(payload));
 
   let r = await fetch(webhook_url, {
     method: "POST",
     body: form_data
   })
+  return await r.json();
 }
